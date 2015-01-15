3a9ce7954c5e1a5a49476b86916c0c0887023e6c
Ryan Mills <ryan@ryanmills.net>
2015-01-15 10:57:53 -0800

    update cache key

--- system/modules/editor2/editor2.mod.php
+++ system/modules/editor2/editor2.mod.php
@@ -14,7 +14,7 @@ class Editor2 extends Module {
         if (\CMS::$_page_type == 'zpage') {
             //\Html::set('{scripts}', '<script src="{root_doc}system/inc/js/jquery.tmpl.min.js" type="text/javascript"></script>');
             \Html::set('{scripts}', '<script src="{root_doc}system/modules/editor2/assets/ckeditor.js" type="text/javascript"></script>');
-            \Html::set('{scripts}', '<script src="{root_doc}system/modules/editor2/assets/config.js" type="text/javascript"></script>');
+            \Html::set('{scripts}', '<script src="{root_doc}system/modules/editor2/assets/config.js?cache=2" type="text/javascript"></script>');
             \Page\Admin::add_quick_link('<li><a id="editor-edit-button" href="#">Enable Rich Editor</a></li>');
             \Html::set('{footer}', self::block('template.html'));
         }
