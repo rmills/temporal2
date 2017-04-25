53403220ad0bec7a318d17fa6cba7a8dab098f26
Ryan Mills <ryan@ryanmills.net>
2017-04-25 16:04:54 -0700

    force https://

--- system/modules/editor3/editor3.mod.php
+++ system/modules/editor3/editor3.mod.php
@@ -13,7 +13,7 @@ class Editor3 extends Module {
     public static function add_links() {
         if (\CMS::$_page_type == 'zpage') {
             //\Html::set('{scripts}', '<script src="{root_doc}system/inc/js/jquery.tmpl.min.js" type="text/javascript"></script>');
-            \Html::set('{scripts}', '<script src="//cdn.ckeditor.com/4.4.7/full/ckeditor.js"></script>');
+            \Html::set('{scripts}', '<script src="https://cdn.ckeditor.com/4.4.7/full/ckeditor.js"></script>');
             \Html::set('{scripts}', '<script src="{root_doc}system/modules/editor3/assets/config.js?cache=2" type="text/javascript"></script>');
             \Page\Admin::add_quick_link('<li><a id="editor-edit-button" href="#">Enable Rich Editor</a></li>');
             \Html::set('{footer}', self::block('template.html'));
